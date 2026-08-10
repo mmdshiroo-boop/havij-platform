99649b4ab5124c501eea8799a31a23308092a744
mohammadshirvani <noroncomputer@gmail.com>
2026-08-10 15:27:01 +0330

    upd

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -78,7 +78,7 @@ app.use(cookieParser());
 app.use(
   fileUpload({
     createParentPath: true,
-    limits: { fileSize: 200 * 1024 * 1024 }, // ۲۰۰ مگابایت 
+    limits: { fileSize: 50 * 1024 * 1024 }, // ۵۰ مگابایت
     useTempFiles: false,
     abortOnLimit: true,
   }),
